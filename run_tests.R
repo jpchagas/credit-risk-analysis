3ddef4aae35530ee1402af83d1e15b70a796aa5b
João Pedro Chagas <jpchagas@gmail.com>
2020-07-27 17:11:38 -0300

    Change validation mode

--- run_tests.R
+++ run_tests.R
@@ -1,9 +1,4 @@
 library(testthat)
-library(shinytest)
-
-test_that("Application works", {
-  # Use compareImages=FALSE because the expected image screenshots were created
-  # on a Mac, and they will differ from screenshots taken on the CI platform,
-  # which runs on Linux.
-  expect_pass(testApp(".", compareImages = FALSE))
-})
+#library(shinytest)
+source("Fibo.R")
+test_results <- test_dir("tests/", reporter="summary")
--- tests/test.R
+++
@@ -1,8 +0,0 @@
-app <- ShinyDriver$new("..")
-app$snapshotInit("test")
-
-app$snapshot()
-app$setInputs(bins = 50)
-app$snapshot()
-app$setInputs(bins = 1)
-app$snapshot()
